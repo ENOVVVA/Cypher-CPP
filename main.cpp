165f8bae02fc88ce5050b729baaff207ed2ae8f1
Timur Jesur <timurjes@gmail.com>
2020-07-23 03:28:44 +0300

    Start
    Starting project, adding headers and class Menu

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Menu{
+    
+    public:
+    void showMain(){
+        cout << "\
+ //===========================================================\\\\\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+||                                                             ||\n\
+ \\\\===========================================================//\
+";
+    }
+};
+Menu menu;
+
+
+void main(){
+menu.showMain();
+
+}
